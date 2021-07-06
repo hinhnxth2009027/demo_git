25b7cb91162dfc9e8615f3e0ed38fcabbf3788a3
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-07-06 18:50:07 +0700

    change ajax to xhr http request

--- demo_jquery_day2/cloudinarycuadet.js
+++ demo_jquery_day2/cloudinarycuadet.js
@@ -1,23 +1,50 @@
-$('.chon_avatar').click(function () {
-    $('.avatar').click()
-})
-$('.chon_cover_photo').click(function () {
-    $('.cover_photo').click()
-})
-var cloudname = 'datgutpoy'
-var key = 'datgoodpoy'
-$('.avatar').change(function () {
-var file = this.files[0]
-    var form_data = new FormData()
-    form_data.append('upload_preset',key)
-    form_data.append('tags','browser_upload')
-    form_data.append('file', file)
-    $.ajax({
-        type:'POST',
-        url:`https://api.cloudinary.com/v1_1/datgutpoy/image/upload`,
-        dataType:'jsonp',
-        data:form_data,
-        success: function (data) {
-            alert(data)
-        }})
+document.addEventListener('DOMContentLoaded', function () {
+
+    $('.avatar').change(function () {
+        var file = this.files[0]
+        var key = 'datgoodpoy'
+        var url = `https://api.cloudinary.com/v1_1/datgutpoy/image/upload`
+        var xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function (){
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    var dataJson = JSON.parse(this.responseText)
+                    $('.show_avatar').attr('src',`${dataJson.url}`)
+                }
+            }
+        }
+        xhr.open('POST', url, true)
+        var data = new FormData()
+        data.append('upload_preset',key)
+        data.append('tags','browser_upload')
+        data.append('file',file)
+        xhr.send(data)
+    })
+    $('.chon_avatar').click(function () {
+        $('.avatar').click()
+    })
+    $('.chon_cover_photo').click(function () {
+        $('.cover_photo').click()
+    })
+
+    $('.cover_photo').change(function () {
+        var file = this.files[0]
+        var key = 'datgoodpoy'
+        var url = `https://api.cloudinary.com/v1_1/datgutpoy/image/upload`
+        var xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function (){
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    var dataJson = JSON.parse(this.responseText)
+                    $('.dat').attr('src',`${dataJson.url}`)
+                }
+            }
+        }
+        xhr.open('POST', url, true)
+        var data = new FormData()
+        data.append('upload_preset',key)
+        data.append('tags','browser_upload')
+        data.append('file',file)
+        xhr.send(data)
+    })
 })
